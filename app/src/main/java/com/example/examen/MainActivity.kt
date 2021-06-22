409bd70fc30014a71ca19cc9639b088b61d49f94
Joel Mercado <joelmercado@Joels-MacBook-Air.local>
2021-06-21 19:34:22 -0500

    Adding kotli extension

--- app/src/main/java/com/example/examen/MainActivity.kt
+++ app/src/main/java/com/example/examen/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.examen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            Log.d(TAG, "Click on Button with find: ${tv_find.text}")
+        }
+
     }
 }
